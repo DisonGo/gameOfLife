d3d79efc8fd96a46a1c34cd27448f14c700af7ab
Evgeny <jokerj224@gmail.com>
2022-09-11 23:04:49 +0300

    master. More cleaning.

--- js/Presets/prepare4Game.js
+++ js/Presets/prepare4Game.js
@@ -1,7 +1,6 @@
 "use strict";
 let aHeight = document.documentElement.clientHeight;
 let aWidth = document.documentElement.clientWidth;
-console.log(aHeight);
 document.body.style.height = aHeight + "px";
 document.body.style.width = aWidth + "px";
 document.body.style.overflow = "hidden";
